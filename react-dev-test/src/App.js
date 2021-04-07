72ce817bd6da5e583d3d02d9b6bf0fd0cba1a72e
Chyntia <chyntiachyn7@gmail.com>
2021-04-07 08:36:40 +0700

    setup routes

--- react-dev-test/src/App.js
+++ react-dev-test/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+import BaseRouter from './routes';
+
+const App = () => (
+    <Router>
+        <BaseRouter />
+    </Router>
+);
 
 export default App;
